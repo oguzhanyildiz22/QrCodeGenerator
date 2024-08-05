da2f7af8ec2956cfcfd281e44abe8fbe3a0c7eef
Oguzhan Yıldız <oguzhan.yildiz@32bit.com.tr>
2024-08-05 17:06:02 +0300

    Added qr code generator codes.

--- src/main/java/com/example/qrcode/controller/QrCodeController.java
+++ src/main/java/com/example/qrcode/controller/QrCodeController.java
@@ -1,12 +1,36 @@
 package com.example.qrcode.controller;
 
+import com.example.qrcode.service.QrCodeGenerator;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Base64;
+
 @RestController
-@RequestMapping("/api/qr")
+@RequestMapping("/api/qr-code")
+@RequiredArgsConstructor
 public class QrCodeController {
 
+    private final QrCodeGenerator qrCodeGenerator;
 
+    private static final String MY_GITHUB = "https://github.com/oguzhanyildiz22";
+    //private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/static/img/QRCode.png";
+
+    @GetMapping("get")
+    public void getQrCode(HttpServletResponse response) throws IOException, WriterException {
+
+        response.setContentType("image/png");
+        BufferedImage bufferedImage = qrCodeGenerator.generateQrCodeImageToShowImage(MY_GITHUB,250,250);
+        ImageIO.write(bufferedImage,"PNG",response.getOutputStream());
+
+    }
+    
 }
---
+++ src/main/java/com/example/qrcode/service/QrCodeGenerator.java
@@ -0,0 +1,42 @@
+package com.example.qrcode.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+@Service
+@RequiredArgsConstructor
+public class QrCodeGenerator {
+
+    public void generateQrCodeImageForPath(String text, int width, int height, String filePath) throws WriterException, IOException {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    public BufferedImage generateQrCodeImageToShowImage(String text, int width, int height) throws WriterException, IOException {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+
+
+
+
+}
